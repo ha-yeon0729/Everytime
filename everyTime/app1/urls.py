6121f81b4fa8be8bfd25e13ad93447366f81a12c
ha-yeon0729 <qorgkdus12@naver.com>
2021-11-06 19:31:06 +0900

    회원가입정보를 DB에 저장+로그인세션기능 구현

--- everyTime/app1/urls.py
+++ everyTime/app1/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-    path('', views.login),
+    path('',views.index),
+    path('login/', views.login),
     path('select/', views.select, name='select'),
     path('signup/', views.signup, name='signup'),
 ]
--- everyTime/app1/views.py
+++ everyTime/app1/views.py
@@ -6,8 +6,11 @@ from selenium import webdriver
 from django.views.decorators.csrf import csrf_exempt
 from time import sleep
 
+from django.contrib.auth.models import User
+from django.contrib import auth
 from django.contrib import messages
 from .models import member
+from django.http import HttpResponse
 
 #def f_login(request):
     #회원가입 db완성 후 로그인 시 id pw 받아와서 db랑 비교 후 일치 하면 다음 페이지로이동 아닐 시 예외 처리
@@ -15,7 +18,34 @@ from .models import member
 @csrf_exempt
 #------------------------------------시간표 crawling 부분(selenium)-------------------------------------------------
 
-def login(request): #추후 시간표 크롤링함수로 이름 변경
+def index(request):
+    return render(request,"index.html")
+
+def login(request):
+    if request.method=="POST":
+        userid=request.POST["userid"]
+        userpw=request.POST["userpw"]
+        #해당 userid,userpw와 일치하는 user객체를 가져온다.
+        user=auth.authenticate(request,username=userid,password=userpw)
+
+        #해당 user 객체가 존재한다면
+        if user is not None:
+            request.session['Id']=userid
+            auth.login(request,user)    #로그인한다
+            print("로그인 성공")
+            return redirect('select')
+
+        #존재하지 않는다면
+        else:
+            # 딕셔너리에 에러메세지를 전달하고 다시 login.html화면으로 돌아감
+            return render(request,'login.html'),{'error':'로그인 정보가 잘못되었습니다!'}
+
+    # 처음 이 페이지로 왔을 때
+    else:
+        return render(request,'login.html')
+
+
+def crawling(request): #추후 시간표 크롤링함수로 이름 변경
     if request.method == "POST":
         def start2time(start):
             base = 900
@@ -107,10 +137,21 @@ def login(request): #추후 시간표 크롤링함수로 이름 변경
 
 @csrf_exempt
 def select(request):
+    userid=request.session.get('Id')
+    if userid:
+        ID=member.objects.get(ssgId=userid)
+        print(ID)
+
+        #Id.ssgId 하면 member DB의 ssgId(FG 아이디) 가 나오고, Id.ssgPw하면 DB의 FG비번이 나온다.
+        #그냥 ID만 쓰면 제일 첫번째 값인 etaId가 나온다.
+        return render(request,'select.html',{'ID':ID.ssgId})
+
     return render(request,"select.html")
+
 
 @csrf_exempt
 def signup(request):
+    # 입력한 아이디, 비번으로 진짜 에타에 접속이 되는지 확인 -하연
     if 'check' in request.POST:
         # 로그인 url
         url = 'https://everytime.kr/login'
@@ -121,7 +162,6 @@ def signup(request):
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
         # 개발용-코드
-
         user_id = request.POST.get('etaid')  # 개인 아이디 입력
         print("2")
         user_pw = request.POST.get('etapw')  # 비밀번호 입력
@@ -147,24 +187,42 @@ def signup(request):
             driver.find_element_by_xpath('//*[@id="menu"]/li[2]/a').click()
             messages.error(request,'에브리타임 아이디와 비밀번호가 확인되었습니다!')
             print("4")
+            return render(request,'signup.html',{'etaid': user_id,'etapw':user_pw})
+
         except:
             driver.quit()
             #return 'err'
-            messages.error(request, '에브리타임에 로그인 할 수 없습니다!')
+            messages.error(request, '유효한 에브리타임 정보가 아닙니다!')
+            return render(request, "check.html")
+        driver.quit()
+
+    # 에타 인증 후 FG 사이트 아이디, 비번 설정
+    elif 'signup' in request.POST:
+        etaid=request.POST["etaid"]
+        etapw=request.POST["etapw"]
+        ssgid = request.POST["ssgid"]
+        ssgpw = request.POST["ssgpw"]
+        ressgpw = request.POST["ressgpw"]
+        if (ssgpw == ressgpw):
+            print("비번이랑 재입력 비번이랑 일치함!")
+            messages.error(request, '정상적으로 회원가입이 되었습니다!')
+
+            #장고에서 제공하는 DB에 FD정보만 저장(로그인기능 편리함)
+            user=User.objects.create_user(username=ssgid,password=ssgpw)
+            #내가 만든 DB(에타정보, FD정보 둘다 저장)
+            DB=member(etaId=etaid,etaPw=etapw,ssgId=ssgid,ssgPw=ssgpw)
+            DB.save()
+
+            auth.login(request,user)
+            return redirect('/')
+
+        else:
+            messages.error(request, '비밀번호가 다릅니다!')
             return render(request, "signup.html")
 
-        driver.quit()
-        print("5")
-        messages.error(request, '정상적으로 회원가입이 되었습니다!')
-        return redirect('/signup')
+    else:  # 맨 처음에 이 페이지로 들어올 때 -하연
+        return render(request,"check.html")
 
-    else:
-        return render(request,"signup.html")
-
-
-@csrf_exempt
-def check(request):
-    return render(request,"check.html")
 
 @csrf_exempt
 def authentic(request):
